68c14e6dcc70dfbb372e00de74496daa1445adb0
Mark Nguyen <marknguyen1621@gmail.com>
2019-06-09 18:17:07 -0400

    Changed copyright footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 
 <!-- Footer -->
-<footer class="footer text-color--white"> 
+<footer class="footer text-color--white">
     <div class="container text-center">
-        <a href="http://www.marknguyen.org">Design by Mark Nguyen</a>
+        <a href="/admin">©2019 BRUSHED</a> | <a href="http://www.marknguyen.org">DESIGN BY MARK NGUYEN</a>
     </div>
 </footer>
 <!--// Footer -->
