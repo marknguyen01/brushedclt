723af90d4d858dc49c3faee12a8c0124b2df9a5c
Mark Nguyen <marknguyen1621@gmail.com>
2019-07-25 11:21:41 -0400

    Fixed contact section overflown

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -31,7 +31,7 @@
 async defer></script>
           </div>
           <div class="col-lg-6 align-self-center">
-            <div class="contact-content py-5 text-center text-lg-left px-0 px-lg-5 row align-items-center">
+            <div class="contact-content py-5 text-center text-lg-left px-0 px-lg-5 row align-items-center no-gutters">
               <div class="contact-info w-100">
                   <div class="contact-info__title">
                       <ion-icon name="ios-time"class="align-middle mr-1"></ion-icon>Operating Hours:
