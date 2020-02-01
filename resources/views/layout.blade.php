bd7bf6b2cda16ebd2e2036f593d3af8aef2ae453
Mark Nguyen <marknguyen1621@gmail.com>
2020-01-31 21:57:09 -0500

    Removing caching for now

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <title>{{ setting('site.title') }}@yield('title')</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
